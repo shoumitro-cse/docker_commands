489db1faa22985054257bf0b54c51d810775571a
shoumitro-cse <shoumitro26@gmail.com>
2021-11-21 23:20:24 +0600

    cmd added

--- elasticsearch/cmd.sh
+++ elasticsearch/cmd.sh
@@ -5,3 +5,11 @@ docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elas
 
 # visit it
 # http://localhost:9200/
+
+docker run -p 9300:9200 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.15.2
+
+docker start 243d2ac4e574 # start docker container
+
+# visit it
+# http://localhost:9300/
+
